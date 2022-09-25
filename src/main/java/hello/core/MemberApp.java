9e999aa82783830f6b73b5112657955053a5e5c6
D-w-nJ <jdu2121@ajou.ac.kr>
2022-09-25 18:03:27 +0900

    trasforming to Spring

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,21 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.order.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class  MemberApp {
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // @bean 지정된걸 다 컨테이너에 넣어준다.
+        //이제는 스프링 컨테이너를 통해 객체를 가져온다.
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        //"memberService" 객체를 가져올거고 타입은 MemberService야
+
         Member memberA = new Member(1L, "memberA", Grade.BASIC);
         memberService.join(memberA);
 
--- src/main/java/hello/core/order/AppConfig.java
+++ src/main/java/hello/core/order/AppConfig.java
@@ -1,17 +1,34 @@
 package hello.core.order;
 
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberserviceImpl;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration //app의 설정정보
 public class AppConfig {
 
+    @Bean // 스프링 컨테이너에 등록이 됨
     public MemberService memberService() {
-        return new MemberserviceImpl(new MemoryMemberRepository());
+        return new MemberserviceImpl(memberRepository());
     }
 
+    @Bean
+    public MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy(); // 할인정책 바꾸기(구성영역만 수정하는것, 사용영역의 어떤 부분도 수정 x)
     }
 }
